9c90da0667c244958db09d5d16acbd67b04264c1
SHIVAMGURJAR0000 <dheerajgurjar55556@gmail.com>
2025-05-11 10:53:37 +0530

    add post route

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ app.get("/", (req, res) => {
   res.send("render is working");
 });
 
+app.post("/getData", (req, res) => {
+  const data = req.body.data;
+  res.json({
+    status: "true",
+    data: data,
+    message: "Data received successfully",
+  });
+});
 app.listen(PORT, () => {
   console.log("server is running......");
 });
