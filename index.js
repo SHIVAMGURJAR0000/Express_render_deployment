5b9d9924790cb7c83042f1de9d2875485e27b2ab
SHIVAMGURJAR0000 <dheerajgurjar55556@gmail.com>
2025-05-11 11:08:02 +0530

    add logic in post route

--- index.js
+++ index.js
@@ -15,11 +15,38 @@ app.get("/", (req, res) => {
 
 app.post("/getData", (req, res) => {
   const data = req.body.data;
-  res.json({
-    status: "true",
-    data: data,
-    message: "Data received successfully",
+
+  if (!Array.isArray(data)) {
+    return res
+      .status(400)
+      .json({ status: "error", message: "Data must be an array." });
+  }
+
+  const numbers = [];
+  const alphabets = [];
+
+  data.forEach((item) => {
+    if (!isNaN(item)) {
+      numbers.push(item);
+    } else {
+      alphabets.push(item);
+    }
   });
+
+  res.status(200).json({
+    status: "success",
+    user_id: "Shivam_Gurjar_07042003",
+    email_id: "shivamgurjar220386@acropolis.in",
+    college_roll_number: "0827CY221057",
+    numbers,
+    alphabets,
+  });
+
+  //   res.json({
+  //     status: "true",
+  //     data: data,
+  //     message: "Data received successfully",
+  //   });
 });
 app.listen(PORT, () => {
   console.log("server is running......");
