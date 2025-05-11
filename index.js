594c55d259edeff29cc1a0bd8437749d2eaf9d53
SHIVAMGURJAR0000 <dheerajgurjar55556@gmail.com>
2025-05-11 10:35:50 +0530

    render testing

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+const cors = require("cors");
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("render is working");
+});
+
+app.listen(PORT, () => {
+  console.log("server is running.....");
+});
